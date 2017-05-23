6cc740286f862d320456a4b95e85e4c05ba0e8e4
ElijahChat <eli@krischatelain.com>
2017-05-23 16:21:31 -0600

    fixed?

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,8 +6,8 @@ $(function() {
     dataType: 'jsonp',
     success: function(response) {
       // handle response
-      for(var i = 0; i < response.courses.completed; i++) {
-        $('#badges').html('<div class="course"><h3>' + response.courses.completed.title + '</h3><img src=' + response.courses.completed.badge + '><a href=' + response.courses.completed.url + ' target="_blank class="btn btn-primary">See Course</a></div>');
+        for(var i = 0; i < response.courses.completed.length; i++) {
+          $('#badges').html('<div class="course"><h3>' + response.courses.completed[i].title + '</h3><img src=' + response.courses.completed[i].badge + '><a href=' + response.courses[i].completed.url + ' target="_blank class="btn btn-primary">See Course</a></div>');
       }
     }
   });
