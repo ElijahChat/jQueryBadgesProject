14c6d814384eb7d7cfd7ba82c549c93ad362f430
ElijahChat <eli@krischatelain.com>
2017-05-23 16:55:20 -0600

    fa

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,8 +6,8 @@ $(function() {
     dataType: 'jsonp',
     success: function(response) {
       // handle response
-      console.log(response);
-      var course;
+        console.log(response);
+        var course;
         for(var i = 0; i < response.courses.completed.length; i++) {
           course = response.courses.completed[i];
           $('#badges').html('<div class="course"><h3>' + course.title + '</h3><img src=' + course.badge + '><a href=' + course.url + ' target="_blank class="btn btn-primary">See Course</a></div>');
