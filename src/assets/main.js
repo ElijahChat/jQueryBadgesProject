9d2724ffc5daca26547f36d0a3dd0b11937e31d6
ElijahChat <eli@krischatelain.com>
2017-05-22 20:06:27 -0600

    ?

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,14 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/3762192.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      for(var i = 0; i < courses.completed; i++) {
+        $('#badges').appendChild('<div class="course"><h3>' + courses.completed.title + '</h3><img src=' + courses.completed.badge + '><a href=' + courses.completed.url + ' target="_blank class="btn btn-primary">See Course</a></div>');
+      }
+    }
+  });
 });
